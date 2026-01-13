127eb6cfec4aaa4a13e45a57c333c971a94ca3d6
Cesar Giovanny Pardo <cgpdatalab@proton.me>
2026-01-13 13:37:38 -0500

    Fix: Eliminado debug mode para produccion

--- app.py
+++ app.py
@@ -6,7 +6,20 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-if __name__=="__main__":
-    app.run(debug=True)
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "app.py",
+      "use": "@vercel/python"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "app.py"
+    }
+  ]
+}
 
 
